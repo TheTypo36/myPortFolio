4d8ad0a6839a5c8430727b17f3684446407dc514
Ashish Raghuvanshi <thetypo36@gmail.com>
2024-06-03 13:18:53 +0530

    first commit

---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClasses?: string;
+}) => {
+  return (
+    <div>
+      <button className="md:w-60 md:mt-10 relative inline-flex h-12 w-full overflow-hidden bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform hover:-translate-y-1 transition duration-400">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span
+          className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg hover:bg-transparent bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+        >
+          {position === "left" && icon}
+          {title}
+          {position === "right" && icon}
+        </span>
+      </button>
+    </div>
+  );
+};
